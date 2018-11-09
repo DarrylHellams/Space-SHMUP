92e77e3a19c4bb1fca7ae31a426d728363f4c495
DarrylHellams <31867675+DarrylHellams@users.noreply.github.com>
2018-11-09 14:09:41 -0500

    Added BoundsCheck script to Hero

---
+++ Assets/Scripts/BoundsCheck.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// keeps a game object on screen
+/// note that this only works for an orthographic main camera at [0, 0, 0]
+/// </summary>
+
+public class BoundsCheck : MonoBehaviour {
+
+    [Header("Set in Inspector")]
+    public float radius = 1f;
+
+    [Header("Set Dynamically")]
+    public float camWidth;
+    public float camHeight;
+
+    void Awake()
+    {
+        camHeight = Camera.main.orthographicSize;
+        camWidth = camHeight * Camera.main.aspect;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+
+        if (pos.x > camWidth - radius)
+        {
+            pos.x = camWidth - radius;
+        }
+        if(pos.x < -camWidth + radius)
+        {
+            pos.x = -camWidth - radius;
+        }
+        if (pos.y > camHeight - radius)
+        {
+            pos.y = camHeight - radius;
+        }
+        if (pos.y < -camHeight + radius)
+        {
+            pos.y = -camHeight + radius;
+        }
+        transform.position = pos;
+    }
+
+    //draw the bounds in the scene pane using OnDrawGizmos()
+    void OnDrawGizmos()
+    {
+        if (!Application.isPlaying) return;
+        Vector3 boundSize = new Vector3(camWidth * 2, camHeight * 2, 0.1f);
+        Gizmos.DrawWireCube(Vector3.zero, boundSize);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
