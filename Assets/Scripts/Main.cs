8f6e73a2aba5db4f4052302cd73890ce4cb99c4f
DarrylHellams <31867675+DarrylHellams@users.noreply.github.com>
2018-11-27 01:29:20 -0500

    Shooting Revisited (Continue on p598)

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour {
     static public Main S; //singleton for Main
+    static Dictionary <WeaponType, WeaponDefinition> WEAP_DICT;
 
     [Header("Set in Inspector")]
     public GameObject[] prefabEnemies;
     public float enemySpawnPerSecond = 0.5f;
     public float enemyDefaultPadding = 1.5f;
+    public WeaponDefinition[] weaponDefinitions;
 
     private BoundsCheck bndCheck;
 
@@ -19,6 +21,12 @@ public class Main : MonoBehaviour {
         S = this;
         bndCheck = GetComponent<BoundsCheck>();
         Invoke("SpawnEnemy", 1f/enemySpawnPerSecond);
+
+        WEAP_DICT = new Dictionary<WeaponType, WeaponDefinition>();
+        foreach (WeaponDefinition def in weaponDefinitions)
+        {
+            WEAP_DICT[def.type] = def;
+        }
     }
 
     public void SpawnEnemy()
@@ -52,6 +60,24 @@ public class Main : MonoBehaviour {
         SceneManager.LoadScene("_Scene_0");
     }
 
+    /// <summary>
+    /// static function that gets a weapon definition from the WEAP_DICT static
+    /// protected field of the main class
+    /// </summary>
+    /// <returns> the weapon definition or if there is no weapon definition with 
+    /// the weapon type passed in returns a new weapon definition with a 
+    /// weapon type of none..</returns>
+    /// <param name= "wt"> the weapon type of the desired weapon definition</param>
+    
+    static public WeaponDefinition GetWeaponDefinition(WeaponType wt)
+    {
+        if (WEAP_DICT.ContainsKey(wt))
+        {
+            return (WEAP_DICT[wt]);
+        }
+
+        return (new WeaponDefinition());
+    }
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// this is an enum of the various possible weapon types
+/// it also includes a "shield" type to allow a shield power-up
+/// items marked [NI] below are not implemented in the IGDPD book
+/// </summary>
+
+public enum WeaponType
+{
+    none, //the default / no weapon
+    blaster, //a simple blaster
+    spread, //two shots simultaneously
+    phaser, //[NI] shots that move in waves
+    missile, //[NI] homing missiles
+    laser, //[NI] damage over time
+    shield //raise shieldLevel
+}
+
+/// <summary>
+/// the weapon definition class allows you to set the properties
+/// of a specific weapon in the Inspector. the main class has 
+/// an array of weapon definitions that make this possible
+/// </summary>
+
+[System.Serializable]
+public class WeaponDefinition
+{
+    public WeaponType type = WeaponType.none;
+    public string letter;
+    public Color color = Color.white;
+    public GameObject projectilePrefab;
+    public Color projectileColor = Color.white;
+    public float damageOnHit = 0;
+    public float continuousDamage = 0;
+    public float delayBetweenShots = 0;
+    public float velocity = 20;
+}
+
+public class Weapon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
